08a3a73405e74ae5831a7577392509d566b6a6de
khanoass <47032455+khanoass@users.noreply.github.com>
2020-10-06 14:01:10 +0200

    Started homepage html and added some empty files for several pages

--- website/src/home.php
+++ website/src/home.php
@@ -1,15 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> 
+    <link rel="stylesheet" href="style.css">
 
     <title>Home</title>
 </head>
 <body>
+
+    <!-- Container -->
+    <div class="jumbotron vertical-center">
+        <div class="container text-center">
+
+            <!-- Title/subtitle -->
+            <h1 class="h1 mb-3 font-weight-normal">Studeum</h1>
+            <h2 class="h5 mb-5 font-weight-normal">Lorem ipsum dolor sit amet</h2>
+
+            <!-- Login/register buttons -->
+            <div class="text-center mb-5">
+                <div class="btn-group-vertical" role="group" aria-label="Login Register Buttons">
+                    <button type="button" class="btn btn-primary btn-lg">Se connecter</button>
+                    <button type="button" class="btn btn-primary btn-lg">Créer un compte</button>
+                </div>
+            </div>
+
+            <!-- "About" button -->
+            <button type="button" class="btn btn-outline-secondary btn-lg mb-3">Le projet</button>
+
+            <!-- Footer -->
+            <p class="mt-5">© 2020 - Studeum</p>
+        </div>
+    </div>
+
     <!-- JS to work with Bootstrap -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
